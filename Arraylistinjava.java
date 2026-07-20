740ecc3b5bd2b726505cd50ed0d575a039cbf5d7
Sabyasachi <krram368@gmail.com>
2026-07-20 20:54:46 +0530

    ArraylistJAVA

---
+++ Arraylistinjava.java
@@ -0,0 +1,52 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Arraylistinjava {
+    static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(25);
+        arr.add(21);
+        arr.add(18);
+        arr.add(5);
+        arr.add(10);
+
+//        System.out.println(arr.get(0));
+//        arr.set(0,12);
+//        System.out.println(arr.get(0));
+
+//        System.out.println(arr);
+
+
+//        int n = arr.size();
+//        for (int i=0;i<n;i++){
+//            System.out.print(arr.get(i)+" ");
+//        }
+//        System.out.println();
+//        for( int ele : arr ){
+//            System.out.print(ele+" ");
+//
+//        }
+
+//        arr.add(2,35);
+//        System.out.println(arr);
+//        int n = arr.size();
+//        arr.remove(n-1);
+//        System.out.println(arr);
+
+//        Collections.sort(arr);
+//        Collections.reverse(arr);
+
+        //NOPRMALLY FOR REVERSING THE ARRAY
+
+        }
+//        int i=0, j=arr.size()-1;
+//        while(i<j){
+//            int temp=arr.get(i);
+//            arr.set(i,arr.get(j));
+//            arr.set(j,temp);
+//            i++;
+//            j--;
+//    }
+}
